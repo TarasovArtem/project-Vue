5d4ee49b3163b3a2ff0119c79c88d3a1a868a60a
Artem <Hans8883@yandex.ru>
2018-09-19 20:05:31 +0200

    fix

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -25,29 +25,28 @@ export default {
           title: 'Take over world',
           completed: false,
         },
-      ]
-    }
+      ],
+    };
   },
-    methods: {
-        addTodo() {
-            if (this.newTodo.trim().length == 0 ) {
-                return
-            }
-           this.todos.push({
-               id: this.idForTodo,
-               title: this.newTodo,
-               completed: false,
-           })
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length === 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      });
 
-           this.newTodo = ''
-           this.idForTodo++
-        }
-    }
-}
+      this.newTodo = '';
+      this.idForTodo++;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
 .todo-input {
   width: 100%;
   padding: 10px 18px;
